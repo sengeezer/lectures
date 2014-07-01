694eefde68d78d8b51c261139ef4dea0a3371268
Fabio Albertin <albertin@gmail.com>
2014-07-01 01:20:59 +0100

    Finished homework

---
+++ node-1/3-homework/assets/test.js
@@ -0,0 +1,5 @@
+/**
+ * Created by albertin on 01/07/14.
+ */
+
+console.log('this is a test, not a testicle');
--- node-1/3-homework/server.js
+++ node-1/3-homework/server.js
@@ -1,16 +1,43 @@
+/* Variation on static page Node server */
+
+// Load Node libs
 var http = require('http'),
+    url = require("url"),
     path = require('path'),
     fs = require('fs');
+
+// Create the server
 var server = http.createServer();
 
-// var crf = require('../mySolution-2/crf');
+// Support function for sending the proper MIME type
+function detMIME(input) {
+    if (input === '.html' || input === '/' || input === ''){
+        return {"Content-Type": "text/html"};
+    }
+    else if (input === '.css'){
+        return {"Content-Type": "text/css"};
+    }
+    else if (input === '.js'){
+        return {"Content-Type": "application/x-javascript"};
+    }
+}
+
+// With a little help from https://gist.github.com/rpflorence/701407
 
 var onRequest = function handleRequest(req,res) {
 
-    var reqUrl = req.url;
-    var fileToLoad = reqUrl.slice(reqUrl.indexOf('/') + 1, reqUrl.length) + '.html';
+    var uri = url.parse(req.url).pathname;
+
+    // if there's no file extension and we're not at the root level
+    if(path.extname(uri) === '' && uri !== '/'){
+        var fileToLoad = path.join(process.cwd(), uri) + '.html';
+    }
+    else {
+        var fileToLoad = path.join(process.cwd(), uri);
+    }
 
     path.exists(fileToLoad, function(exists) {
+        // if the file isn't there, return a 404
         if (!exists) {
             res.writeHead(404, {"Content-Type": "text/plain"});
             res.write("404 Not Found\n");
@@ -18,21 +45,27 @@ var onRequest = function handleRequest(req,res) {
             return;
         }
 
+        // if the browser wants a directory, he should want index.html
+        if (fs.statSync(fileToLoad).isDirectory()) {
+            fileToLoad += '/index.html';
+        }
+
+        // try reading the file and throw a 500 if unsuccessful
         fs.readFile(fileToLoad, "binary", function(err, file) {
-            if(err) {
+            if (err) {
                 res.writeHead(500, {"Content-Type": "text/plain"});
                 res.write(err + "\n");
                 res.end();
                 return;
             }
-            res.writeHead(200);
+
+            // Write the response using the correct MIME type
+            res.writeHead(200, detMIME(path.extname(uri)));
             res.write(file, "binary");
             res.end();
+        });
     });
 
-
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('You requested ' + req.url + ' so I should give you ' + fileToLoad);
 };
 
 server
