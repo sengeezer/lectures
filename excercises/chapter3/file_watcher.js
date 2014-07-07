d71e390329976483745dede603b7d2cb1580f264
Fabio Albertin <albertin@gmail.com>
2014-07-07 20:59:23 +0200

    Completed file watcher example

--- excercises/chapter3/file_watcher.js
+++ excercises/chapter3/file_watcher.js
@@ -31,7 +31,18 @@ function Watcher(watchDir, processedDir) {
     };
 }
 
-
-
 util.inherits(Watcher, events.EventEmitter);
+
+var watcher = new Watcher(watchDir, processedDir);
+
+watcher.on('process', function process(file) {
+    var watchFile      = this.watchDir + '/' + file;
+    var processedFile  = this.processedDir + '/' + file.toLowerCase();
+
+    fs.rename(watchFile, processedFile, function(err) {
+        if (err) throw err;
+    });
+});
+
+watcher.start();
 
