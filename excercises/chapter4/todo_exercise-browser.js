788c0671e4588fa3f6a2ff64ed058665a68a82b1
Fabio Albertin <falbertin@ebay.com>
2014-07-25 18:37:37 +0200

    Progress on chapter 4

--- excercises/chapter4/todo_exercise-browser.js
+++ excercises/chapter4/todo_exercise-browser.js
@@ -3,12 +3,13 @@
  */
 
 var http = require('http');
-// var url = require('url');
+
 var items = [];
+var qs = require('querystring');
 
 var server = http.createServer(function(req, res){
     function show(res){
-        var html = '<html><head><title>To do list</title>title></head>head><body>' +
+        var html = '<html><head><title>To do list</title></head><body>' +
             '<h1>To do list</h1>' +
             '<ul>' +
             items.map(function(item){
@@ -22,6 +23,26 @@ var server = http.createServer(function(req, res){
         res.setHeader('Content-Type', 'text/html');
         res.setHeader('Content-Length', Buffer.byteLength(html));
         res.end(html);
+    }
+    function notFound(res) {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Not Found');
+    }
+    function badRequest(res) {
+        res.statusCode = 400;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Bad Request');
+    }
+    function add(req, res) {
+        var body = '';
+        req.setEncoding('utf8');
+        req.on('data', function(chunk){ body += chunk });
+        req.on('end', function(){
+            var obj = qs.parse(body);
+            items.push(obj.item);
+            show(res);
+        });
     }
     if ('/' == req.url){
         switch(req.method){
